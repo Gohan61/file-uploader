36a587d94cb8a02ea67ca79d4fc7ba5e9de9436f
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-08-19 15:10:35 +0200

    Feat: define middleware for session, errors, data
    * Using prisma session store to persist sessions in database
    * Parsing json/form data with express bodyparser
    * Intercepting thrown errors and returning via object

--- dist/app.js
+++ dist/app.js
@@ -3,11 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const index_1 = require("./routes/index");
-const app = (0, express_1.default)();
+exports.app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-app.use("/", index_1.router);
-app.listen(port, () => {
+exports.app.use("/", index_1.router);
+exports.app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
+});
+exports.app.use((err, req, res, next) => {
+    res.status(err.status | 500);
+    res.send({ error: err });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,49 @@
-import express, { Express, Request, Response, Application } from "express";
+import express, {
+  Express,
+  Request,
+  Response,
+  Application,
+  NextFunction,
+} from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { router } from "./routes/index";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
+import session from "express-session";
+import passport from "passport";
+import "./config/passport";
 
-const app: Application = express();
-const port = process.env.PORT || 3000;
+export const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: process.env.secret as string,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+app.use(passport.session());
 
 app.use("/", router);
 
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status | 500);
+  res.send({ error: err });
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
