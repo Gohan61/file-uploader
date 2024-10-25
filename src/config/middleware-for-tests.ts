8b986cae75a76153f63d8ae666c589057f18baf9
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-10-25 09:21:35 +0200

    Doc: load middleware for tests via module
    * To keep tests more clean

---
+++ src/config/middleware-for-tests.ts
@@ -0,0 +1,33 @@
+import { fileRouter } from "../routes/files";
+import express, { Express } from "express";
+import { PrismaClient } from "@prisma/client";
+import "../config/passport";
+import { router } from "../routes";
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import passport from "passport";
+
+export default function middleWare(app: Express) {
+  app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ extended: false }));
+
+  app.use(
+    session({
+      cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+      },
+      secret: process.env.secret as string,
+      resave: true,
+      saveUninitialized: true,
+      store: new PrismaSessionStore(new PrismaClient(), {
+        checkPeriod: 2 * 60 * 1000, //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }),
+    })
+  );
+  app.use(passport.session());
+
+  app.use("/", router);
+  app.use("/files", passport.authenticate("session"), fileRouter);
+}
