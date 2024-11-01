9b5cbbb1ec6177ec427f7b2b1cac696f52d866d3
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-11-01 18:49:07 +0100

    Feat: add routes Homepage, Signin, Signup pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./components/App";
+import HomePage from "./components/Homepage";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "signup", element: <Signup /> },
+      { path: "signin", element: <Signin /> },
+    ],
   },
 ]);
 
