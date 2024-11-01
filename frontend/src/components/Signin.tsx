59fddaba1773763fe04ed68c47a1549d26b07da1
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-11-01 18:49:52 +0100

    Feat: render Signin form + handle errors

---
+++ frontend/src/components/Signin.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+
+export default function Signin() {
+  const [signinForm, setSigninForm] = useState({ username: "", password: "" });
+  const [error, setError] = useState({ user: "", password: "" });
+  const [inputValidation, setValidation] = useState({
+    username: "",
+    password: "",
+  });
+
+  function focusValidation(element: string) {
+    if (signinForm.username === "" && element === "username") {
+      setValidation({ username: "Username cannot be empty", password: "" });
+    } else if (signinForm.password === "" && element === "password") {
+      setValidation({
+        ...inputValidation,
+        password: "Password cannot be empty",
+      });
+    }
+  }
+
+  function handleSignin(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    let respStatus: number;
+    setError({ user: "", password: "" });
+
+    fetch("http://localhost:3000/signin", {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: signinForm.username,
+        password: signinForm.password,
+      }),
+    })
+      .then((res) => {
+        respStatus = res.status;
+        return res.json();
+      })
+      .then((res) => {
+        if (respStatus === 200) {
+          console.log("ok");
+        } else {
+          console.log(res);
+
+          throw res.errors;
+        }
+      })
+      .catch((err) => {
+        if (err instanceof Object) {
+          setError({ user: "", password: err.message });
+        } else {
+          setError({ user: err, password: "" });
+        }
+      });
+  }
+
+  return (
+    <div>
+      <h2>Sign in</h2>
+
+      <form action="" method="POST" encType="multipart/form-data">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={signinForm.username}
+          required
+          onChange={(e) => {
+            setSigninForm({ ...signinForm, username: e.target.value });
+            setValidation({ ...inputValidation, username: "" });
+          }}
+          onBlur={() => focusValidation("username")}
+        />
+        <p>{inputValidation.username}</p>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={signinForm.password}
+          required
+          onChange={(e) => {
+            setSigninForm({ ...signinForm, password: e.target.value });
+            setValidation({ ...inputValidation, password: "" });
+          }}
+          onBlur={() => focusValidation("password")}
+        />
+        {<p>{inputValidation.password}</p>}
+        {error.password ? <p>{error.password}</p> : ""}
+        {error.user ? <p>{error.user}</p> : ""}
+        <button onClick={(e) => handleSignin(e)}>Sign in</button>
+      </form>
+    </div>
+  );
+}
