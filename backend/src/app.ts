8efd8b516fc38bc3e5d4b3cf7d5395b643cca298
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-11-05 16:27:12 +0100

    Refactor: set credentials + origin CORS
    * In order to set cookie on frontend, origin + credentials need to be
    set for CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,10 +18,9 @@ import "./config/passport";
 
 export const app: Application = express();
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(
   session({
     cookie: {
