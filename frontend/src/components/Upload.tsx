55be1c13842ac457233dc02412d6db35b99958a9
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-11-18 18:27:19 +0100

    Style: hide/show form + render column layout
    * To make it easier to read, form is in column layout
    * To prevent form taking up too much room, it is collapsable
    * Button and text styling

--- frontend/src/components/Upload.tsx
+++ frontend/src/components/Upload.tsx
@@ -8,6 +8,7 @@ export default function Upload() {
   const [uploadMessage, setUploadMessage] = useState("");
   const [error, setError] = useState("");
   const [fileInputKey, setFileInputKey] = useState<number>(0);
+  const [collapse, setCollapse] = useState(false);
 
   const {
     uploadFolder,
@@ -62,7 +63,7 @@ export default function Upload() {
       .then((res) => {
         if (respStatus === 200) {
           setFile(null);
-          setUploadMessage("Upload succesfull");
+          setUploadMessage("Upload successful");
           (() => setTimeout(() => setUploadMessage(""), 5000))();
           setFileInputKey(Date.now());
         } else {
@@ -80,37 +81,80 @@ export default function Upload() {
   }
 
   return (
-    <div className="flex flex-row w-fit">
-      <form action="" method="POST" encType="multipart/form-data">
-        <label htmlFor="file">Click to pick a new file</label>
-        <input
-          type="file"
-          name="file"
-          id="file"
-          key={fileInputKey}
-          required
-          onChange={(e) => handleFile(e)}
-        />
-        <label htmlFor="folder">Select a folder</label>
-        <select
-          name="folder"
-          id="folder"
-          value={uploadFolder}
-          onChange={(e) => setUploadFolder(Number(e.target.value))}
+    <div className="flex flex-column flex-wrap justify-center p-3 pt-5 bg-sky-50 w-screen z-10 relative">
+      {collapse ? (
+        <>
+          <form
+            action=""
+            method="POST"
+            encType="multipart/form-data"
+            className="flex flex-column w-[300px] flex-wrap gap-2"
+          >
+            <label htmlFor="file" className="font-bold text-lg">
+              Click to pick a new file
+            </label>
+            <input
+              type="file"
+              name="file"
+              id="file"
+              key={fileInputKey}
+              required
+              onChange={(e) => {
+                handleFile(e);
+                setError("");
+              }}
+              className="file:rounded-md file:bg-slate-600 file:text-white italic"
+            />
+            <label htmlFor="folder" className="text-lg">
+              Select a folder
+            </label>
+            <select
+              name="folder"
+              id="folder"
+              value={uploadFolder}
+              onChange={(e) => setUploadFolder(Number(e.target.value))}
+              className="rounded-md border"
+            >
+              {folderList.folders.map((folder) => {
+                return (
+                  <option key={folder.id} value={folder.id}>
+                    {folder.title}
+                  </option>
+                );
+              })}
+            </select>
+            <button
+              onClick={(e) => uploadFile(e)}
+              className="px-2 py-1 border-2 rounded-md h-fit bg-sky-800 text-white"
+            >
+              Upload new file
+            </button>
+          </form>
+          {loading ? <p>Loading</p> : ""}
+          {uploadMessage ? (
+            <p className="mt-2 w-full text-center">{uploadMessage}</p>
+          ) : (
+            ""
+          )}
+          {error ? <p className="mt-2 w-full text-center">{error}</p> : ""}
+          <button
+            onClick={() => setCollapse(false)}
+            className="absolute top-5 right-5"
+          >
+            {"Collapse"}
+          </button>
+        </>
+      ) : (
+        <button
+          onClick={() => {
+            setCollapse(true);
+            setError("");
+          }}
+          className="text-lg px-2 py-1 border-2 rounded-md h-fit bg-sky-800 text-white"
         >
-          {folderList.folders.map((folder) => {
-            return (
-              <option key={folder.id} value={folder.id}>
-                {folder.title}
-              </option>
-            );
-          })}
-        </select>
-        <button onClick={(e) => uploadFile(e)}>Upload new file</button>
-      </form>
-      {loading ? <p>Loading</p> : ""}
-      {uploadMessage ? <p>{uploadMessage}</p> : ""}
-      {error ? <p>{error}</p> : ""}
+          Upload
+        </button>
+      )}
     </div>
   );
 }
