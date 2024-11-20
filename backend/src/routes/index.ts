d0c7e130002ba7f6d0e8e6e6a647cc7810d1d36d
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-11-20 09:07:31 +0100

    Feat: rate limit signin requests
    * To prevent forcing of malicious signin, requests are rate limited on
    signin

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,19 @@
 import express, { Router, Request, Response } from "express";
 import { logout, signin, signup } from "../controllers/login-out";
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+  handler: (req, res, next, options) =>
+    res
+      .status((options.statusCode = 429))
+      .json({ errors: "Too many requests, please try again in 10 minutes" }),
+});
 
 export const router: Router = express.Router();
 
-router.post("/signin", signin);
+router.post("/signin", limiter, signin);
 
 router.post("/signup", signup);
 
